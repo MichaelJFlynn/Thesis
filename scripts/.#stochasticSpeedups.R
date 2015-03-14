0da16c9e83883e1496b37141ecfa1b02d078a000
Michael Flynn <michaelflynn@rna.williams.edu>
2015-03-14 19:55:25 -0400

    added data and scripts

---
+++ scripts/.#stochasticSpeedups.R
@@ -0,0 +1,1 @@
+michaelflynn@rna.williams.edu.80549
---
+++ scripts/stochasticSpeedups.R
@@ -0,0 +1,5 @@
+library(ggplot2)
+
+data1 = read.table("../data/test_results1.txt", head = TRUE)
+data2 = read.table("../data/test_results2.txt", head = TRUE)
+
