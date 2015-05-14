8fcf01693019a7b55a0d69b63fc7a571f1de9fec
Michael Flynn <michaelflynn@rna.williams.edu>
2015-05-14 03:20:21 -0400

    Fixed reactivityanalysis script

--- scripts/ReactivityAnalysis/ReactivityAnalysis.sh
+++ scripts/ReactivityAnalysis/ReactivityAnalysis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-./pnest bistable.seq >> pnestOutput
-python PijToPi.py compat.pairs >> compat.pi
-python PijToPi.py noncompat.pairs >> noncompat.pi
+./pnest bistable.seq > pnestOutput
+python PijToPi.py compat.pairs > compat.pi
+python PijToPi.py noncompat.pairs > noncompat.pi
